c075b9b2a134a45c089a956b8bb5975fe370808f
Laure Thompson <laurejt@gmail.com>
2012-01-27 06:45:23 -0800

    Added boxcat.

--- src/web/index.php
+++ src/web/index.php
@@ -4,11 +4,10 @@
  */
 
 require_once 'includes/common.php';
-require_once 'services/test.php';
-require_once 'views/test.php';
+require_once 'views/index.php';
 
 page_header('Index');
-page_body(service_get_data());
+page_body();
 page_footer();
 
 ?>
--- src/web/views/index.php
+++ src/web/views/index.php
@@ -1,6 +1,32 @@
 <?php
+/**
+ * Home page view.
+ * 
+ * @author Laure Thompson <laurejt@cs.washington.edu>
+ */
 
-include('../404.shtml');
-die();
+set_include_path(get_include_path() . PATH_SEPARATOR . '../');
 
+// File should never be requested directly
+if (basename(getcwd()) == basename(dirname(__FILE__)))
+{
+	include('404.shtml');
+	die();
+}
+
+//-----------------------------------------------------------------------------
+// Functions
+//-----------------------------------------------------------------------------
+
+/**
+ * Generates the page body for this page.
+ *
+ * @param array $data Data for the page.
+ */
+function page_body($data = null)
+{
+?>
+			<img src="images/boxcat.jpg" alt="B0XC47!" />
+<?php
+}
 ?>
