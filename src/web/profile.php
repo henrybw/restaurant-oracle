6b4b5a9807043bf090ceb30907636983f0c802ac
Henry Baba-Weiss <htw@cs.washington.edu>
2012-01-27 10:17:28 -0800

    Fixed the "delayed Log Out appearing" bug.

--- src/web/profile.php
+++ src/web/profile.php
@@ -9,8 +9,12 @@ require_once 'includes/common.php';
 require_once 'services/profile.php';
 require_once 'views/profile.php';
 
+// Called before page_header() is called so the nav bar can be
+// appropriately updated to correctly display "Log Out".
+$data = service_get_data();
+
 page_header('Profile');
-page_body(service_get_data());
+page_body($data);
 page_footer();
 
 ?>
