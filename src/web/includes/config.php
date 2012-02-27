162076d454f1647fb4f9e954ed38743717df2e33
Henry Baba-Weiss <htw@cs.washington.edu>
2012-02-26 17:47:23 -0800

    Changed database login credentials to be configured for John's server by default.

--- src/web/includes/config.php
+++ src/web/includes/config.php
@@ -14,7 +14,14 @@ if (basename(getcwd()) == basename(dirname(__FILE__)))
 	die();
 }
 
-$db_name = 'htw_restaurant_oracle';
+// Credentials for Cubist
+/*$db_name = 'htw_restaurant_oracle';
 $db_user = 'htw';
-$db_pass = '7RRcs7dp';
+$db_pass = '7RRcs7dp';*/
 
+// Credentials for ChickenFactory.net (John's server)
+$db_name = 'chicken2_boxcat';
+$db_user = 'chicken2_boxcat';
+$db_pass = '#yTkUB{XE)y~';
+
+?>
